9387076639abb3312c46726da358c66b798ef9d2
panupongjkn <panupong.jkn@gmail.com>
2021-01-30 03:24:13 +0700

    test 2

--- src/main/java/com/example/MessageController.java
+++ src/main/java/com/example/MessageController.java
@@ -24,7 +24,7 @@ public class MessageController {
 
     @GetMapping("/")
     public String getHello() {
-        return "Hello World test";
+        return "Hello World";
     }
 
     @GetMapping("/messages")
